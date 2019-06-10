d605a3b45e0fa5295cc7cbfeb583bd719b154b61
Hun Lee <dan.hun.lee@gmail.com>
2019-06-10 02:49:55 -0700

    hl

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TriviaGame
+// In this homework, I had to make a trivia game. Basically you start by clicking the "start" button and a number of questions will pop up. Then you try to answer the quetions within a time limit. After time runs out or you click "Done" then it will take you to the result page where it shows how many you got correct or not. 
+
+//TODO: I have not finished the part for counting the correct, incorrect, and unanswered answers. 
+
+//TODO: I have not finished the styling of the page using css. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,16 +8,31 @@
 window.onload = function() {
     $("#start").on("click", start);
     $("#done").on("click", done);
-
+    $(".form-check-input1").hide();
+    $(".form-check-input2").hide();
+    $(".form-check-input3").hide();
+    $(".form-check-input4").hide();
+    $(".form-check-input5").hide();
+    $(".form-check-input6").hide();
+    $(".form-check-input7").hide();
+    $(".form-check-input8").hide();
+    $(".form-check-input9").hide();
+    $(".form-check-input10").hide();
+    $(".form-check-input11").hide();
+    $(".form-check-input12").hide();
 
   };
-  
+  var correctAns = 0;
+  var incorrectAns = 0;
+  var unAns = 0;
+  var Seahawks;
+
   //  Variable that will hold our setInterval that runs the stopwatch
   var intervalId;
   
   // prevents the clock from being sped up unnecessarily
   var clockRunning = false;
-  var time = 5;
+  var time = 31;
 
   function start() {
   
@@ -27,39 +42,68 @@ window.onload = function() {
       clockRunning = true;
     }
     $("#start").hide();
+    $(".form-check-input1").show();
+    $(".form-check-input2").show();
+    $(".form-check-input3").show();
+    $(".form-check-input4").show();
+    $(".form-check-input5").show();
+    $(".form-check-input6").show();
+    $(".form-check-input7").show();
+    $(".form-check-input8").show();
+    $(".form-check-input9").show();
+    $(".form-check-input10").show();
+    $(".form-check-input11").show();
+    $(".form-check-input12").show();
 
-   
     $(".q1").prepend("<p>Which team is the best NFL team?</p>");
     $(".form-check-label1").prepend("<p>Seahawks</p>");
     $(".form-check-label2").prepend("<p>Patriots</p>");
     $(".form-check-label3").prepend("<p>Browns</p>");
     $(".form-check-label4").prepend("<p>Packers</p>");
-
-
+    
     $(".q2").prepend("<p>Who is the best quarterback in NFL?</p>");
     $(".form-check-label5").prepend("<p>Tom Brady</p>");
     $(".form-check-label6").prepend("<p>Aaron Rodgers</p>");
     $(".form-check-label7").prepend("<p>Philip Rivers</p>");
     $(".form-check-label8").prepend("<p>Russell Wilson</p>");
+
     $(".q3").prepend("<p>Who is the best head coach in NFL?</p>");
     $(".form-check-label9").prepend("<p>Bill Belichick</p>");
     $(".form-check-label10").prepend("<p>Andy Reid</p>");
     $(".form-check-label11").prepend("<p>Pete Carroll</p>");
     $(".form-check-label12").prepend("<p>Doug Pederson</p>");
+    
+    //TODO: I need to work on the counting the correct answer, incorrect answer and unanswerd questions! 
+
   }
 
   function done() {
+    $(".form-check-input1").hide();
+    $(".form-check-input2").hide();
+    $(".form-check-input3").hide();
+    $(".form-check-input4").hide();
+    $(".form-check-input5").hide();
+    $(".form-check-input6").hide();
+    $(".form-check-input7").hide();
+    $(".form-check-input8").hide();
+    $(".form-check-input9").hide();
+    $(".form-check-input10").hide();
+    $(".form-check-input11").hide();
+    $(".form-check-input12").hide();
+
     
       $(".q1").hide();
       $(".form-check-label1").hide();
       $(".form-check-label2").hide();
       $(".form-check-label3").hide();
       $(".form-check-label4").hide();
+      
       $(".q2").hide();
       $(".form-check-label5").hide();
       $(".form-check-label6").hide();
       $(".form-check-label7").hide();
       $(".form-check-label8").hide();
+      
       $(".q3").hide();
       $(".form-check-label9").hide();
       $(".form-check-label10").hide();
@@ -67,9 +111,9 @@ window.onload = function() {
       $(".form-check-label12").hide();
 
       $(".q4").prepend("<p>ALL DONE!</p>");
-      $(".q5").prepend("<p>Correct Answer: </p>");
-      $(".q6").prepend("<p>Incorrect Answer: </p>");
-      $(".q7").prepend("<p>Unanswered: </p>");
+      $(".q5").prepend("<p>Correct Answer: " + correctAns + "</p>");
+      $(".q6").prepend("<p>Incorrect Answer: "+ incorrectAns + "</p>");
+      $(".q7").prepend("<p>Unanswered: "+ unAns + "</p>");
 
   }
   
